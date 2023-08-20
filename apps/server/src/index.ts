bb98cce467bcb795f1f56123e8b2bcf67a296aa5
ryanrobinson1 <45307703+ryanrobinson1@users.noreply.github.com>
2023-08-20 23:29:19 +0100

    remove docs package and replace with barebones server package

---
+++ apps/server/src/index.ts
@@ -0,0 +1,1 @@
+console.log("hello from server... hello 2x");
---
+++ apps/server/src/tests/index.spec.ts
@@ -0,0 +1,6 @@
+describe("hello first test", () => {
+  it("more loving", () => {
+    const vars = { hello: "money face" };
+    console.log("first test more going here man", vars);
+  });
+});
